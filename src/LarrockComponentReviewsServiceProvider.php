10f2ae3e330c1c8f944004c771a3c37ae2db829b
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-16 14:38:03 +1000

    update

--- src/LarrockComponentReviewsServiceProvider.php
+++ src/LarrockComponentReviewsServiceProvider.php
@@ -17,6 +17,7 @@ class LarrockComponentReviewsServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
         $this->loadViewsFrom(__DIR__.'/views', 'larrock');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/vendor/larrock')
@@ -35,18 +36,6 @@ class LarrockComponentReviewsServiceProvider extends ServiceProvider
             return new $class;
         });
         $this->app->make(ReviewRequest::class);
-
-        $timestamp = date('Y_m_d_His', time());
-        $timestamp_after = date('Y_m_d_His', time()+10);
-        $migrations = [];
-        if ( !class_exists('CreateReviewsTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_create_reviews_table.php'] = database_path('migrations/'.$timestamp.'_create_reviews_table.php');
-        }
-        if ( !class_exists('AddForeignKeysToReviewsTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_add_foreign_keys_to_reviews_table.php'] = database_path('migrations/'.$timestamp_after.'_add_foreign_keys_to_reviews_table.php');
-        }
-
-        $this->publishes($migrations, 'migrations');
 
         $this->app['router']->aliasMiddleware('ReviewsMiddleware', ReviewsMiddleware::class);
     }
