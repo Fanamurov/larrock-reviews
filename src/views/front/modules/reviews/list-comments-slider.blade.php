28f246cf3896b69e15d60b858c1c1b06d6f5fa24
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-13 12:20:45 +1000

    update slider template

--- src/views/front/modules/reviews/list-comments-slider.blade.php
+++ src/views/front/modules/reviews/list-comments-slider.blade.php
@@ -3,15 +3,15 @@
     <div class="uk-width-1-1">
         <section id="reviews" class="uk-margin-bottom">
             @php
-                $datetime1 = new DateTime('2016-11-22');
+                $datetime1 = new DateTime('2017-06-22');
                 $datetime2 = new DateTime(date('Y-m-d'));
                 $interval = $datetime1->diff($datetime2);
-                $counter = 348 + (35*$interval->format('%R%a'));
+                $counter = 14 + (5*$interval->format('%R%a'));
             @endphp
             <p class="uk-h2 uk-text-center">Уже <i>{{ $counter }}</i> довольных клиентов<br/>были обслужены нами</p>
-            <div class="uk-push-1-5 uk-width-4-5 uk-width-small-4-5 uk-width-medium-3-5">
+            <div>
                 <div class="uk-slidenav-position" data-uk-slideset="{default: 1; autoplay: true}">
-                    <ul class="uk-grid uk-slideset" id="reviews-grid">
+                    <ul class="uk-grid uk-slideset uk-push-1-10 uk-width-8-10 uk-width-small-8-10 uk-width-medium-8-10" id="reviews-grid">
                         @foreach($reviews as $item)
                             <li>
                                 {!! $item->comment !!}
