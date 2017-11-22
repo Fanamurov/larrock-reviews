9b72c35b09dc6739f553ab448e2b7457ba219295
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-22 17:25:47 +1000

    hot fix

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,7 @@
 <?php
 
-Route::group(['middleware' => $middlewares], function(){
-    Route::post('/reviews/post', 'Larrock\ComponentReviews\ReviewsController@post')->name('reviews.post');
-    Route::get('/reviews', 'Larrock\ComponentReviews\ReviewsController@index')->name('reviews.all');
-});
+Route::post('/reviews/post', 'Larrock\ComponentReviews\ReviewsController@post')->name('reviews.post');
+Route::get('/reviews', 'Larrock\ComponentReviews\ReviewsController@index')->name('reviews.all');
 
 Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData', 'SiteSearchAdmin']], function(){
     Route::resource('reviews', 'Larrock\ComponentReviews\AdminReviewsController');
